1e245644423eda605e7db11ef67a987c9e2b6c27
Benoit Tremblay <benoit.tremblay@ec.gc.ca>
2025-11-04 15:39:45 -0500

    Add plots for validation purposes.

--- forward/evaluation/validation.py
+++ forward/evaluation/validation.py
@@ -27,7 +27,6 @@ def main(config: DictConfig) -> None:
     # Load test set results (predictions)
     logger.info("Load test set results...")
     pred = instantiate(config.loader.stage.test.results.hofx.load)
-    ref = np.load('../forward_outputs/forward_emulator/runs/version_000/test_hofx.npy').astype(np.float32)
 
     # Load test set references
     logger.info("Load test set references...")
@@ -39,14 +38,14 @@ def main(config: DictConfig) -> None:
     # Plots
     logger.info("Plot comparison...")
     fig1 = fig_rmse_bars(hofx, pred, clear, x_range=[[0, 1.0], [0, 2.0]],
-                         title=["(c) Retrained model - Forward model errors",
-                                "(d) Retrained model - Normalized forward model errors"])
+                         title=["(a) Forward model - Forward model errors",
+                                "(b) Forward model - Normalized forward model errors"])
     save_plot(fig1, config.paths.run_dir + '/Figure3_rmse_bars_test.png')
     fig2 = fig_rmse_bars2(hofx, pred, clear, daytime, x_range=[[0, 1.5], [0, 2.0]],
-                          title=["(c) Retrained model - Daytime forward model errors",
-                                 "(d) Retrained model - Nighttime forward model errors"])
+                          title=["(a) Forward model - Daytime forward model errors",
+                                 "(b) Forward model - Nighttime forward model errors"])
     save_plot(fig2, config.paths.run_dir + '/Figure2_rmse_bars2_test.png')
-    fig3 = fig_errs_by_channel(hofx, pred, ref=ref, title=[f'Channel {i+7}' for i in range(hofx.shape[1])],
+    fig3 = fig_errs_by_channel(hofx, pred, title=[f'Channel {i+7}' for i in range(hofx.shape[1])],
                                orientation='horizontal', x_range=[[0, 7.0], [0, 0.9], [0, 1.1], [0, 1.3], [0, 1.9],
                                                                   [0, 1.0], [0, 1.6], [0, 1.8], [0, 2.1], [0, 1.6]])
     save_plot(fig3, config.paths.run_dir + '/Figure4_errs_by_channel_vert_test.png')
