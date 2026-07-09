5b5a9788110e5aa0ca32ab77639c3d47318cda2e
tremblaybenoit <benoit.tremblay.23@gmail.com>
2026-07-09 16:06:08 -0400

    Experiment with new layout.

---
+++ src/loader/dataloader.py
@@ -0,0 +1,156 @@
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from omegaconf import DictConfig
+from utilities.instantiators import instantiate
+import os
+import torch.multiprocessing as mp
+# Set this BEFORE any dataloader starts
+mp.set_sharing_strategy('file_system')
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
+
+class BaseDataloader(pl.LightningDataModule):
+    def __init__(self, batch_size: int = 32, num_workers: int | None = None,
+                 persistent_workers: bool = True, pin_memory: bool = True, shuffle: bool = True) -> None:
+        """ Base dataloader class.
+
+        Parameters
+        ----------
+        batch_size : int. Batch size for the dataloader.
+        num_workers : int. Number of workers for the dataloader.
+        persistent_workers : bool. If True, the data loader will keep workers alive between epochs.
+        pin_memory : bool. If True, the data loader will copy Tensors into CUDA pinned memory before returning them.
+        shuffle : bool. If True, the data loader will shuffle the data at every epoch.
+
+        Returns
+        -------
+        None.
+        """
+
+        #  Class inheritance
+        super().__init__()
+
+        # Number of cpus
+        self.num_workers = num_workers if num_workers is not None else os.cpu_count() // 2
+        # Persistent workers for faster data loading
+        self.persistent_workers = persistent_workers
+        # Neural network training batch size
+        self.batch_size = batch_size
+        # Pin memory for faster data transfer
+        self.pin_memory = pin_memory
+        # Shuffle data at every epoch
+        self.shuffle = shuffle
+
+        # Datasets
+        self.ds_train = None
+        self.ds_valid = None
+        self.ds_test = None
+        self.ds_pred = None
+
+    def train_dataloader(self) -> DataLoader:
+        """ Loads training set.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            Training set (inputs & outputs).
+
+        """
+        return DataLoader(self.ds_train, batch_size=self.batch_size, num_workers=self.num_workers,
+                          pin_memory=self.pin_memory, persistent_workers=self.persistent_workers, shuffle=self.shuffle)
+
+    def val_dataloader(self) -> DataLoader:
+        """ Load validation set.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            Validation set (inputs & outputs).
+
+        """
+        return DataLoader(self.ds_valid, batch_size=self.batch_size, num_workers=self.num_workers,
+                          pin_memory=self.pin_memory, persistent_workers=self.persistent_workers)
+
+    def test_dataloader(self) -> DataLoader:
+        """ Load test set.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            Test set (inputs & outputs).
+
+        """
+        return DataLoader(self.ds_test, batch_size=self.batch_size, num_workers=self.num_workers,
+                          pin_memory=self.pin_memory, persistent_workers=self.persistent_workers)
+
+    def predict_dataloader(self) -> DataLoader:
+        """ Load prediction set.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            Prediction set (inputs & outputs if available).
+
+        """
+        return DataLoader(self.ds_pred, batch_size=self.batch_size, num_workers=self.num_workers,
+                          pin_memory=self.pin_memory, persistent_workers=self.persistent_workers)
+
+
+class Dataloader(BaseDataloader):
+    def __init__(self, stage: DictConfig, batch_size: int = 32, num_workers: int | None = None,
+                 persistent_workers: bool = True, pin_memory: bool = True) -> None:
+        """ Dataloader for the CRTM dataset.
+
+        Parameters
+        ----------
+        stage: DictConfig. Configuration object for the dataset at each stage (train, valid, test, pred).
+        batch_size : int. Batch size for the dataloader.
+        num_workers : int. Number of workers for the dataloader.
+        persistent_workers : bool. If True, the data loader will not shut down the worker processes after a dataset has been consumed.
+        pin_memory : bool. If True, the data loader will copy Tensors into CUDA pinned memory before returning them.
+
+        Returns
+        -------
+        None.
+        """
+
+        #  Class inheritance
+        super().__init__(batch_size=batch_size, num_workers=num_workers, persistent_workers=persistent_workers, pin_memory=pin_memory)
+
+        # Data sets
+        self.ds_stage = stage
+
+    def setup(self, stage: str):
+        """ Set up the dataset for training, validation, testing, or prediction.
+
+            Parameters
+            ----------
+            stage : str. Stage of the model ('train', 'valid', 'test', 'predict').
+
+            Returns
+            -------
+            None.
+        """
+
+        # Load datasets
+        if stage == 'train':
+            # Training/validation data
+            self.ds_train, self.ds_valid = instantiate(self.ds_stage.train), instantiate(self.ds_stage.valid)
+        elif stage == 'test':
+            # Test/prediction data
+            self.ds_test = instantiate(self.ds_stage.test)
+        elif stage == 'pred':
+            # Prediction data
+            self.ds_pred = instantiate(self.ds_stage.predict)
